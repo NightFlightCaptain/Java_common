8a34899421a0de4aa57219268cdf8f246af5e122
xhwanhaoran <whr1314520@qq.com>
2019-07-13 18:27:34 +0800

    代码的鲁棒性-反转链表 ReverseList：递归做法，如果确定了某一个节点用不到，可以考虑只是用一个节点完成全部任务

--- main/java/algorithm/JianZhiOffer/ReverseList.java
+++ main/java/algorithm/JianZhiOffer/ReverseList.java
@@ -9,10 +9,21 @@ package algorithm.JianZhiOffer;
 public class ReverseList {
 	public ListNode reverseList(ListNode head) {
 
+		if (head==null||head.next == null) {
+			return head;
+		}
+		//每次返回的节点都是末尾节点不变
+		ListNode node = reverseList(head.next);
+		//这两步保证当前节点的下一节点的下一节点指向自己，也就是反转
+		head.next.next=head;
+		head.next=null;
 
+		return node;
+
+		/*
 		ListNode pre = null;
 		ListNode next = null;
-		while (head != null) {/*循环中做的就是标记下一节点，然后将当前节点指向上一节点*/
+		while (head != null) {//循环中做的就是标记下一节点，然后将当前节点指向上一节点
 			next = head.next;
 			head.next = pre;
 
@@ -20,6 +31,7 @@ public class ReverseList {
 			head = next;
 		}
 		return pre;
+		*/
 	}
 
 	public static void main(String[] args) {
