b7e796e16d50c06e9ab8e5ac9a557722c563faa6
xhwanhaoran <whr1314520@qq.com>
2019-08-27 14:32:13 +0800

    CodeInterviewGuide11 程序员面试算法题 【未排序数组中累加和为给定值的最长子数组系列问题补2】

--- main/java/algorithm/CodeInterviewGuide/CodeInterviewGuide11.java
+++ main/java/algorithm/CodeInterviewGuide/CodeInterviewGuide11.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 /**
  * @author: 小栗旬
- * @Date: 2019/8/27 11:24
+ * @Date: 2019/8/27 14:18
  * <p>
  * 题目描述
- * 给定一个无序数组arr，其中元素可正、可负、可0。求arr所有子数组中正数与负数个数相等的最长子数组的长度。
+ * 给定一个无序数组arr，其中元素只能是1或0。求arr所有的子数组中0和1个数相等的最长子数组的长度
  * [要求]
  * 时间复杂度为O(n)，空间复杂度为O(n)
  * <p>
@@ -18,15 +18,14 @@ import java.util.Scanner;
  * <p>
  * 输出描述:
  * 输出一个整数表示答案
- * <p>
  * 示例1
  * 输入
  * 5
- * 1 -2 1 1 1
+ * 1 0 1 0 1
  * 输出
- * 2
+ * 4
  */
-public class CodeInterviewGuide10 {
+public class CodeInterviewGuide11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
@@ -36,17 +35,17 @@ public class CodeInterviewGuide10 {
         }
         HashMap<Integer, Integer> map = new HashMap<>(N);
         map.put(0,-1);
-        int current =0;
-        int maxCount =0;
+        int totalCount = 0;
+        int currentCount = 0;
         for (int i = 0; i < N; i++) {
-            current +=Integer.compare(nums[i],0);
-            if (map.containsKey(current)){
-                maxCount = Math.max(maxCount,i-map.get(current));
+            currentCount += nums[i] == 1 ? 1 : -1;
+            if (map.containsKey(currentCount)){
+                totalCount = Math.max(totalCount,i-map.get(currentCount));
             }else {
-                map.put(current,i);
+                map.put(currentCount,i);
             }
         }
-        System.out.println(maxCount);
-
+        System.out.println(totalCount);
     }
+
 }
