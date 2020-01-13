b9edf5292efdc2364c0a5595977418c6de6f3997
xhwanhaoran <whr1314520@qq.com>
2020-01-13 14:50:30 +0800

    LeetCode第17周双周夜猫周赛 LeetCode5143[解压缩编码列表]、LeetCode5144[矩阵区域和]、LeetCode5145[祖父节点值为偶数的节点和]、LeetCode5146[不同的循环子字符串] 5146不会做

---
+++ main/java/algorithm/leetcode/competition/weekdouble17/LeetCode5145.java
@@ -0,0 +1,116 @@
+package algorithm.leetcode.competition.weekdouble17;
+
+import algorithm.JianZhiOffer.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 【祖父节点值为偶数的节点和】
+ * <p>
+ * 给你一棵二叉树，请你返回满足以下条件的所有节点的值之和：
+ * <p>
+ * 该节点的祖父节点的值为偶数。（一个节点的祖父节点是指该节点的父节点的父节点。）
+ * 如果不存在祖父节点值为偶数的节点，那么返回 0 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+ * 输出：18
+ * 解释：图中红色节点的祖父节点的值为偶数，蓝色节点为这些红色节点的祖父节点。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点的数目在 1 到 10^4 之间。
+ * 每个节点的值在 1 到 100 之间。
+ *
+ * @author: 小栗旬
+ * @Date: 2020/1/11 23:47
+ */
+public class LeetCode5145 {
+    public int sumEvenGrandparent(TreeNode root) {
+        return dfsMethod(root);
+    }
+
+    int sum;
+    /* dfs版本 */
+
+    private int dfsMethod(TreeNode root) {
+        sum =0;
+        dfsHelper(root,-1,-1);
+        return sum;
+    }
+
+    private void dfsHelper(TreeNode root, int fa, int gfa) {
+        if (root == null) {
+            return;
+        }
+        if (gfa != -1 && ((gfa & 1) == 0)) {
+            sum += root.val;
+        }
+        dfsHelper(root.left,root.val,fa);
+        dfsHelper(root.right,root.val,fa);
+    }
+
+
+    /*  自己写的版本 */
+
+    private int cycleMethod(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return 0;
+        }
+        queue.add(root);
+        if (root.left != null) {
+            queue.add(root.left);
+        }
+        if (root.right != null) {
+            queue.add(root.right);
+        }
+        int res = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                if (node.left.left != null) {
+                    if ((node.val & 1) == 0) {
+                        res += node.left.left.val;
+                    }
+                    queue.add(node.left.left);
+                }
+                if (node.left.right != null) {
+                    if ((node.val & 1) == 0) {
+                        res += node.left.right.val;
+                    }
+                    queue.add(node.left.right);
+                }
+            }
+            if (node.right != null) {
+                if (node.right.left != null) {
+                    if ((node.val & 1) == 0) {
+                        res += node.right.left.val;
+                    }
+                    queue.add(node.right.left);
+                }
+                if (node.right.right != null) {
+                    if ((node.val & 1) == 0) {
+                        res += node.right.right.val;
+                    }
+                    queue.add(node.right.right);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LeetCode5145 solution = new LeetCode5145();
+        System.out.println(solution.sumEvenGrandparent(TreeNode.getTreeNode(new int[]{6, 7, 8, 2, 7, 1, 3, 9, 0, 1, 4, 0, 0, 0, 5}))==18);
+        System.out.println(solution.sumEvenGrandparent(TreeNode.getTreeNode(new int[]{50, 0, 54, 98, 6, 0, 0, 0, 34})));
+        System.out.println(solution.sumEvenGrandparent(TreeNode.getTreeNode(new int[]{79, 52, 86, 71, 12, 0, 2, 0, 0, 0, 81, 91, 1, 0, 0, 0, 0, 0, 93})));
+    }
+}
---
+++ main/java/algorithm/leetcode/competition/weekdouble17/LeetCode5146.java
@@ -0,0 +1,34 @@
+package algorithm.leetcode.competition.weekdouble17;
+
+/**
+ * 【不同的循环子字符串】
+ * <p>
+ * 给你一个字符串 text ，请你返回满足下述条件的 不同 非空子字符串的数目：这些子字符串可以写成某个字符串与其自身的串联。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：text = "abcabcabc"
+ * 输出：3
+ * 解释：3 个子字符串分别为 "abcabc" ， "bcabca" 和 "cabcab" 。
+ * 示例 2：
+ * <p>
+ * 输入：text = "leetcodeleetcode"
+ * 输出：2
+ * 解释：2 个子字符串为 "ee" 和 "leetcodeleetcode" 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= text.length <= 2000
+ * text 只包含小写英文字母。
+ *
+ * @author: 小栗旬
+ * @Date: 2020/1/11 23:04
+ */
+public class LeetCode5146 {
+//    public int distinctEchoSubstrings(String text) {
+//
+//    }
+}
